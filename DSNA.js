9b00dc45062e6215091b3068578614c8ad6cec05
a-wong-8 <awong88@berkeley.edu>
2023-08-22 16:47:21 -0700

    robot inventory

--- DSNA.js
+++ DSNA.js
@@ -1,17 +1,43 @@
 function robotInventory(log) {
     let inventory = {};
+    const revenue = [];
     
     log.map(arr => {
-        if (arr[0] === 'supply') {
-            inventory[arr[1]] = 1
-        } else if (arr[0] === 'upgrade') {
-            
-        } else if (arr[0] === 'sell') {
+        const transType = arr[0];
+
+        if (transType === 'supply') {
+            const robotName = arr[1];
+            const count = parseInt(arr[2]);
+            const price = parseInt(arr[3]);
+
+            if (!inventory[arr[1]]) inventory[arr[1]] = [];
+
+            inventory[robotName].push(...Array(count).fill(price));
+        
+        } else if (transType === 'sell') {
+            const robotName = arr[1];
+            const count = parseInt(arr[2]);
+            let revenueFromSell = 0;
+
+            if (inventory[robotName] && inventory[robotName].length >= count) {
+                const prices = inventory[robotName].sort((a,b)=>a-b);
+                for (let i = 0; i < count; i ++) {
+                    revenueFromSell += prices.shift();
+                }
+                revenue.push(revenueFromSell);
+                inventory[robotName] = prices;
+            }
+
+        } else if (transType === 'upgrade') {
+            const robotName = arr[1];
+            const count = parseInt(arr[2]);
+            const oldPrices = arr[3].map(price=>)
+            const newPrices = arr[4]
             
         }
     })
-    
-    return inventory
+
+    return revenue
 }
 
 logs = [
