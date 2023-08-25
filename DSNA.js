1035413339b45bcb743ac3fbc1b6232c17ad37c4
a-wong-8 <awong88@berkeley.edu>
2023-08-25 14:49:47 -0700

    start matrix rotate

--- DSNA.js
+++ DSNA.js
@@ -53,4 +53,10 @@ logs = [
     ['sell', 'robot2', '1']
 ]
 
-console.log(robotInventory(logs)) // [100, 60, 60, 100]
+// console.log(robotInventory(logs)) // [100, 60, 60, 100]
+
+function matrixRotate(matrix) {
+
+}
+
+console.log(matrixRotate([[1,2,3],[4,5,6],[7,8,9]]));
