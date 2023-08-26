63a3f461214af353e9719262fbd4a55e67fb228a
a-wong-8 <awong88@berkeley.edu>
2023-08-26 10:07:05 -0700

    matrix rotate in js done

--- DSNA.js
+++ DSNA.js
@@ -56,7 +56,18 @@ logs = [
 // console.log(robotInventory(logs)) // [100, 60, 60, 100]
 
 function matrixRotate(matrix) {
+    row = matrix.length;
+    col = matrix[0].length;
+    final = new Array(col).fill(0).map(()=> new Array(row).fill(0));
 
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < col; j++) {
+            final[j][row - 1 - i] = matrix[i][j]
+        }
+    }
+    return final;
 }
 
-console.log(matrixRotate([[1,2,3],[4,5,6],[7,8,9]]));
+console.log(matrixRotate([[1,2,3],[4,5,6],[7,8,9]])); // [7,4,1] [8,5,2] [9,6,3]
+
+// console.log(matrixRotate([[6,4,1,3],[4,8,7,5],[1,12,9,6],[2,4,1,2]])); // [2,1,4,6] [4,12,8,4] [1,9,7,1] [2,6,5,3]
