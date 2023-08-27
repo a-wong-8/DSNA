0c52b50dba4f1962a1e3835192be885a7d3483b9
a-wong-8 <awong88@berkeley.edu>
2023-08-26 18:03:04 -0700

    min diff done

--- DSNA.js
+++ DSNA.js
@@ -68,6 +68,20 @@ function matrixRotate(matrix) {
     return final;
 }
 
-console.log(matrixRotate([[1,2,3],[4,5,6],[7,8,9]])); // [7,4,1] [8,5,2] [9,6,3]
-
+// console.log(matrixRotate([[1,2,3],[4,5,6],[7,8,9]])); // [7,4,1] [8,5,2] [9,6,3]
 // console.log(matrixRotate([[6,4,1,3],[4,8,7,5],[1,12,9,6],[2,4,1,2]])); // [2,1,4,6] [4,12,8,4] [1,9,7,1] [2,6,5,3]
+
+function minDiff(nums, sep) {
+    final = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + sep; j < nums.length; j++) {
+                final.push(Math.abs(nums[i]-nums[j]))
+            }
+        }
+    min = Math.min(...final);       
+    return min;
+}
+
+console.log(minDiff([1,5,4,10,9],3)); // 4
+console.log(minDiff([3,10,5,8],1)); // 2
