84bed24669436238229a13cb4e3e661c347ffa8a
a-wong-8 <awong88@berkeley.edu>
2023-09-25 16:39:04 -0700

    added permutations

--- DSNA.js
+++ DSNA.js
@@ -356,3 +356,29 @@ function oneNum(nums) {
 
 // console.log(oneNum([1,2,2])); // 1
 // console.log(oneNum([1,1,2])); // 2
+
+// Write a recursive function that returns all of the permutations of an array
+// (A permutation is a possible ordering of the elements in an array)
+// e.g. permutations([1,2]) => [[1,2], [2,1]]
+// e.g. permutations([1,2,3]) => [[1,2,3], [1,3,2], [2,1,3], 
+//                                [2,3,1], [3,1,2], [3,2,1]]
+
+function permutations(array) {
+    if (array.length <= 1) return [array];
+  
+    const result = [];
+    const first = array.pop(); // removes last item 
+    const prevPerms = permutations(array);
+  
+    prevPerms.forEach(perm => {
+      for (let i = 0; i <= perm.length; i++) {
+        let nextPerm = perm.slice(0, i).concat([first]).concat(perm.slice(i)); // slice 0 to right, concat [last item], concat slice off left 
+        result.push(nextPerm);
+      }
+    });
+  
+    return result;
+  }
+  
+  // console.log(permutations([1,2,3])); 
+  // console.log(permutations([1,2]));
