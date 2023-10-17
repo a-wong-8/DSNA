759e3c1b49f34661aeb537099dab4925c60d5276
a-wong-8 <awong88@berkeley.edu>
2023-10-17 15:07:29 -0700

    bsearch on rotated arr

--- DSNA2.js
+++ DSNA2.js
@@ -12,20 +12,36 @@ var kClosest = function(points, k) {
 // // We only want the closest k = 1 points from the origin, so the answer is just [[-2,2]].
 // console.log(kClosest([[3,3],[5,-1],[-2,4]], 2)); // Output: [[3,3],[-2,4]] -------------------------------------------------
 
+function bSearchRotatedSortedArr (nums, target) {
+    if (nums.length === 0) return -1;
 
+    let left = 0;
+    let right = nums.length-1;
 
+    while (left <= right) {
+        let mid = Math.floor((left+right) / 2);
 
+        if (nums[mid] === target) return mid;
+    
+        if (nums[left] <= nums[mid]) {
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid -1
+            } else {
+                left = mid +1
+            }
 
+        } else {
 
+            if (nums[mid] < target && target <= nums[right]) {
+                left = mid+1    
+            } else {
+                right = mid-1
+            }
+        }
+    }
+    return -1
+}
 
-
-
-
-
-
-
-
-
-
-
-
+console.log(bSearchRotatedSortedArr([4,5,6,7,0,1,2], 0)); // Output: 4
+console.log(bSearchRotatedSortedArr([4,5,6,7,0,1,2], 3)); // Output: -1
+console.log(bSearchRotatedSortedArr([1], 0)); // Output: -1
