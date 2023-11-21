59efad20f1f942fc2f7b14b78a5695f7d5b2f518
a-wong-8 <awong88@berkeley.edu>
2023-11-20 18:54:56 -0800

    start trees

---
+++ Trees.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+function TreeNode(val, left, right) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+
+var invertTree = function(root) {
+    if (!root) return null;
+
+    let temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+
+    if (root.left) invertTree(root.left);
+    if (root.right) invertTree(root.right);
+
+    return root
+};
+
+console.log(invertTree([4,2,7,1,3,6,9])); // [4,7,2,9,6,3,1]
+
